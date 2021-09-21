44c4fff5f353c4aeb9f42f031c5d8c763b9a34bd
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-09-21 18:37:28 -0400

    done in-class

---
+++ app/src/main/java/ca/tetervak/mycatmessage3a/SecondFragment.kt
@@ -0,0 +1,48 @@
+package ca.tetervak.mycatmessage3a
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import ca.tetervak.mycatmessage3a.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_HometFragment)
+        }
+
+        binding.buttonMyFragment.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_myFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
